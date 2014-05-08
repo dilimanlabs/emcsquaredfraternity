c53277eee4638a85eca1f0a856487143c1b1596b
Francis Chua Gregorio <fcgregorio@gmail.com>
2014-05-08 16:46:14 +0800

    working on the matter at hand

--- handlers/main.py
+++ handlers/main.py
@@ -2,8 +2,6 @@ import os
 import webapp2
 import jinja2
 
-from models import BlogPost
-
 template_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'templates'))
 #template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from google.appengine.ext import ndb
+#from google.appengine.ext import ndb
 
-class BlogPost(ndb.Model):
-	title = ndb.StringProperty()
-	content = ndb.StringProperty()
-	date = ndb.DateTimeProperty(auto_add_now=True)
+#class BlogPost(ndb.Model):
+#	title = ndb.StringProperty()
+#	content = ndb.StringProperty()
+#	date = ndb.DateTimeProperty(auto_add_now=True)
